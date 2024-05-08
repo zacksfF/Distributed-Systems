3ca91ebf143f55294f4bfc63054fa94c86e9a561
zakaria <aamonaze@gmail.com>
2024-05-08 12:11:02 +0100

    fixed some section

--- message-passing/connection.go
+++ message-passing/connection.go
@@ -2,39 +2,40 @@ package messagepassing
 
 import "io"
 
-//Message is the type that client/server send over the wire
-type Message struct{
-	Meta MetaType 
-	OtherClient string
+// Message is the type that client/server send over the wire
+type Message struct {
+	Meta         MetaType
+	OtherClient  string
 	ConnectionID string
-	Data []byte 
+	Data         []byte
 }
 
-//metaType describes the intent of a message --some messgae are meant to simply
-//be dilivered to a connection 
+// metaType describes the intent of a message --some messgae are meant to simply
+// be dilivered to a connection
 type MetaType uint8
 
-//Constants o represent each Meta Type 
+// Constants o represent each Meta Type
 const (
 	MetaNone = MetaType(iota)
-	MetaWAT //Invalid/unkown meta type recieved 
-	MetaNoSuchConnection 
+	MetaWAT  //Invalid/unkown meta type recieved
+	MetaNoSuchConnection
 	MetaUnknownProto
-	MetaClientCLosed 
-	MetaConnACk 
-	MetaConnClosed 
-	MetaAuth 
-	MetaAuthOk 
+	MetaClientCLosed
+	MetaConnSyn
+	MetaConnACk
+	MetaConnClosed
+	MetaAuth
+	MetaAuthOk
 	MetaauthFailure
 )
 
-type Connection interface{
+type Connection interface {
 	io.ReadWriteCloser
 
 	//Reads the content of a single messages sent to us, blocking if necessary.
 	ReadMessage() ([]byte, error)
 
-	//Write a message, quarnteeing that the []byte given is the fill body 
+	//Write a message, quarnteeing that the []byte given is the fill body
 	//of the message.
 	WriteMessage([]byte) error
 
@@ -42,16 +43,16 @@ type Connection interface{
 	OtherClient(string, error)
 }
 
-//NewConnectionhandlers is an interface that client call when a request for 
-//a new Connection comes in .
-type NewConnectionhandler interface{
+// NewConnectionhandlers is an interface that client call when a request for
+// a new Connection comes in .
+type NewConnectionhandler interface {
 	// IncomingConnection. If you violate either of these, accept() panics.
 	IncomingConnection(proto string, accept func() Connection)
 }
 
-//MessageTranslator is type that can read a message from a Reader and write 
-//a message to a writer in some format.
-type MessageTranslator interface{
+// MessageTranslator is type that can read a message from a Reader and write
+// a message to a writer in some format.
+type MessageTranslator interface {
 	ReadMessage() (*Message, error)
 	WriteMessage(*Message) error
 }
